b9372b92924ce169c68272e59ff26f0eed161edc
Adrian Cobo <acoboh.github@gmail.com>
2023-08-19 21:18:30 +0200

    Added new module query-filter-jpa-openapi-3

---
+++ query-filter-jpa-openapi-3/src/main/java/org/query/filter/jpa/openapi/data/jpa/annotations/EnableQueryFilterOpenApi.java
@@ -0,0 +1,33 @@
+package org.query.filter.jpa.openapi.data.jpa.annotations;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Class to configure packages of Filter Class
+ *
+ * @author Adrián Cobo
+ * 
+ */
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface EnableQueryFilterOpenApi {
+
+	/**
+	 * Base packages to find controllers
+	 * 
+	 * @return packages to find controllers
+	 */
+	String[] basePackages() default {};
+
+	/**
+	 * Base package classes to find controllers
+	 * 
+	 * @return package classes to find controllers
+	 */
+	Class<?>[] basePackageClasses() default {};
+
+}

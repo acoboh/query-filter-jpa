48c4b3b97a6fe9a8ed6b57efae6cc9359a8a06b0
Adrian Cobo <acoboh.github@gmail.com>
2023-11-04 13:04:25 +0100

    Removed deprecated annotation on example

--- examples/basic-example-sb-3/src/main/java/io/github/acoboh/query/filter/example/QueryFilterBasicExampleApplication.java
+++ examples/basic-example-sb-3/src/main/java/io/github/acoboh/query/filter/example/QueryFilterBasicExampleApplication.java
@@ -1,6 +1,5 @@
 package io.github.acoboh.query.filter.example;
 
-import org.query.filter.jpa.openapi.data.jpa.annotations.EnableQueryFilterOpenApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,7 +8,6 @@ import io.github.acoboh.query.filter.jpa.annotations.EnableQueryFilter;
 
 @SpringBootApplication
 @EnableQueryFilter(basePackageClasses = PostFilterDef.class)
-@EnableQueryFilterOpenApi
 public class QueryFilterBasicExampleApplication {
 
 	public static void main(String[] args) {
